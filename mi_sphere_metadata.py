90181edba7a57534d8afa2440bf870635682cfcb
Sergej Orlov <wladimirych@gmail.com>
2022-10-18 18:13:38 +0200

    Annotate types

--- mi_sphere_metadata.py
+++ mi_sphere_metadata.py
@@ -4,9 +4,28 @@ import json
 import math
 import struct
 import sys
+from typing import Literal, NamedTuple, TypeAlias, cast
+
+from panoedit import PosePluginResult
+
+Matrix: TypeAlias = tuple[float, float, float, float, float, float, float, float, float]
 
 
-def read_matrix_data(filename):
+class PoseBase(NamedTuple):
+    yaw: float
+    pitch: float
+    roll: float
+
+
+class PoseDegrees(PoseBase):
+    pass
+
+
+class PoseRadians(PoseBase):
+    pass
+
+
+def read_matrix_data(filename: str) -> bytes | None:
     with open(filename, "rb") as f:
         buf = f.read(100000)
         tag_start = bytes([0x86, 0x92, 0x07, 0, 0x24, 0, 0, 0])
@@ -14,17 +33,21 @@ def read_matrix_data(filename):
         if tag_pos == -1:
             return None
         tag_data_offset_position = tag_pos + len(tag_start)
-        offset = struct.unpack(
-            "<I", buf[tag_data_offset_position : tag_data_offset_position + 4]
-        )[0]
+        offset: int
+        [offset] = cast(
+            tuple[int],
+            struct.unpack(
+                "<I", buf[tag_data_offset_position : tag_data_offset_position + 4]
+            ),
+        )
         return buf[offset + 12 : offset + 12 + 36]
 
 
-def decode_matrix(data):
-    return list(struct.unpack("f" * 9, data))
+def decode_matrix(data: bytes) -> Matrix:
+    return cast(Matrix, struct.unpack("f" * 9, data))
 
 
-def matrix_to_angles(ar):
+def matrix_to_angles(ar: Matrix) -> PoseRadians:
     """Calculates angles in radians
      rz * ry * rx
     ⎡cos(y)⋅cos(z)  sin(x)⋅sin(y)⋅cos(z) - sin(z)⋅cos(x)  sin(x)⋅sin(z) + sin(y)⋅cos(x)⋅cos(z) ⎤
@@ -44,52 +67,59 @@ def matrix_to_angles(ar):
         x = math.atan2(-r23, r22)
         y = math.atan2(-r31, cy)
         z = 0
-    return z, x, y
+    return PoseRadians(z, x, y)
 
 
-def get_angles_radians(filename):
+def get_angles_radians(filename: str) -> PoseRadians | None:
     matrix_data = read_matrix_data(filename)
     if matrix_data is None:
         return None
     matrix_values = decode_matrix(matrix_data)
-    yaw, pitch, roll = matrix_to_angles(matrix_values)
-    return yaw, pitch, roll
+    return matrix_to_angles(matrix_values)
 
 
-def get_angles_degrees(filename):
+def get_angles_degrees(filename: str) -> PoseDegrees | None:
     angles_radians = get_angles_radians(filename)
     if angles_radians is None:
         return None
-    return list(map(math.degrees, angles_radians))
+    return PoseDegrees(*map(math.degrees, angles_radians))
 
 
-def show_pose(angles, fmt):
-    yaw, pitch, roll = angles
+def show_pose(angles: PoseDegrees, fmt: str | None) -> None:
     if fmt == "json":
-        print(json.dumps({"yaw": yaw, "pitch": pitch, "roll": roll}))
+        data = {"yaw": angles.yaw, "pitch": angles.pitch, "roll": angles.roll}
+        print(json.dumps(data))
     elif fmt == "short":
-        print("%.2f,%.2f,%.2f" % (yaw, pitch, roll))
+        print("%.2f,%.2f,%.2f" % (angles.yaw, angles.pitch, angles.roll))
     else:
-        print("Yaw: %.2f\nPitch: %.2f\nRoll: %.2f" % (yaw, pitch, roll))
+        print(
+            "Yaw: %.2f\nPitch: %.2f\nRoll: %.2f"
+            % (angles.yaw, angles.pitch, angles.roll)
+        )
 
 
-def write_sidecar_file(image_filename, angles):
-    yaw, pitch, roll = angles
+def write_sidecar_file(image_filename: str, pose: PoseDegrees) -> None:
     sidecar_filename = image_filename + ".pose.json"
-    serialized = json.dumps({"yaw": yaw, "pitch": pitch, "roll": roll})
+    sidecar_data = {"yaw": pose.yaw, "pitch": pose.pitch, "roll": pose.roll}
+    serialized = json.dumps(sidecar_data)
     with open(sidecar_filename, "w", encoding="ascii") as f:
         f.write(serialized)
 
 
-def panoedit_metadata_plugin(filename):
+def panoedit_metadata_plugin(filename: str) -> PosePluginResult | None:
     angles_degrees = get_angles_degrees(filename)
     if angles_degrees is None:
         return None
-    yaw, pitch, roll = angles_degrees
-    return {"pose": {"yaw": yaw, "pitch": pitch, "roll": roll}}
+    return {"pose": {"pitch": angles_degrees.pitch, "roll": angles_degrees.roll}}
 
 
-def main():
+class Args(argparse.Namespace):
+    image: str
+    format: Literal["json", "short"] | None
+    sidecar: str | Literal[True] | None
+
+
+def main() -> None:
     parser = argparse.ArgumentParser(
         description="Display yaw, pitch, roll angles in degrees, extracted from EXIF"
     )
@@ -104,7 +134,7 @@ def main():
             "Provide image name to write sidecar with angles from IMAGE"
         ),
     )
-    conf = parser.parse_args()
+    conf = parser.parse_args(namespace=Args())
     angles = get_angles_degrees(conf.image)
     if angles is None:
         sys.exit("Mi Sphere rotation matrix not found in file %s" % conf.image)
